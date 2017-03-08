d15ac50462c4994c3577ecc079f3fc5fee40c29c
Pranil Vora <pvora7@gatech.edu>
2017-03-08 18:38:56 -0500

    added initial paths for locations

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.engine('html', require('ejs').renderFile);
 app.use('/', routes);
-app.listen(Number(process.env.PORT) || 3000, function () {
-  console.log('testing app!')
-});
+// app.listen(Number(process.env.PORT) || 3000, function () {
+//   console.log('testing app!')
+// });
--- routes.js
+++ routes.js
@@ -1,6 +1,29 @@
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 8080;
+
 var router = express.Router();
 
-router.get('/', (req, res) => res.render('home.html'));
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+router.get('/', (req, res) => res.json({ message: 'hooray! welcome to our api!' }));
+
+
+router.route('/locations')
+  .get(function(req,res) {
+    res.json({"array": ["Subway","Panda Express","Tech Rec","Post Office"]});
+  });
+
+app.use('/api', router);
+app.listen(port);
+console.log('Magic happens on port ' + port);
 
 module.exports = router;
