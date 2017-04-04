0b4ccc63fe799b8513cebe467f870d8b6ab35838
Pranil Vora <pvora7@gatech.edu>
2017-04-03 20:56:41 -0400

    added post route to submit location (lat, long). connected server to firebase db to save these locations.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+var admin = require("firebase");
+
 
 app.engine('html', require('ejs').renderFile);
 app.use('/', routes);
--- routes.js
+++ routes.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("location-analyzer-6ee10-firebase-adminsdk-eidcu-0c54dba12b");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://location-analyzer-6ee10.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("locations");
+var locationsRef = ref.child("Latitudes + Longitudes");
+
+var count = 0;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -20,6 +34,17 @@ router.get('/', (req, res) => res.json({ message: 'Location Analyzer API' }));
 router.route('/locations')
   .get(function(req,res) {
     res.json({"array": ["Subway","Panda Express","Tech Rec","Post Office"]});
+  });
+
+router.route('/submitLocation')
+  .post(function(req,res) {
+    var latitude = req.body.lat;
+    var longitude = req.body.long;
+    count = count +1;;
+    locationsRef.child(count).set({
+      location: latitude + ', ' + longitude
+    });
+    res.send(latitude + ', ' + longitude);
   });
 
 app.use('/api', router);
